3d78a17c04b3886fcb4d6a75582b40616b0e8501
rjsandim <rjsandim@gmail.com>
2015-08-07 18:58:27 -0400

    com atualizador de tabela de processos

--- js/scheduling.js
+++ js/scheduling.js
@@ -11,17 +11,35 @@ $(document).ready(function() {
 		Processo.inicia = $("#inicia-em").val();
 		Processo.id = Math.floor((Math.random() * 10000) + 1);
 		Processo.index = processos.length; 
-		
-		processos.push(Processo);
-		$("#reset").click();
+	
+		if (existeProcesso(Processo)) { 
+			processos.push(Processo);
+			$("#reset").click();
 
-		atualizarTabelaDeProcessos(Processo);
+			atualizarTabelaDeProcessos(Processo);
+		}
 
 		console.log(processos);
 	});
 
+	function existeProcesso(Processo) {
+		return Processo.nome.length && Processo.tamanho.length && Processo.inicia.length;
+	}
+
+
 	function atualizarTabelaDeProcessos(Processo) {
+
+		var itemTabela = "<tr><td></td>";
+		itemTabela += "<td>"+ Processo.index +"</td>";
+		itemTabela += "<td>"+ Processo.nome +"</td>";
+		itemTabela += "<td>"+ Processo.id +"</td>";
+		itemTabela += "<td>"+ Processo.tamanho +"</td>";
+		itemTabela += "<td>"+ Processo.prioridade +"</td>";
+		itemTabela += "<td>"+ Processo.inicia +"</td>";
+		itemTabela += "<td></td><td></td><td></td>";
+		itemTabela += "<td><button>Editar</button> <button>Remover</button></td></tr>";
 		
+		$("#novo-processo").append(itemTabela);
 	}
 
 });
